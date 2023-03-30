9915b51676fdc2245e5ac78c3a0ed64f4618b983
SeraWing <swing@jeemail.com>
2023-03-29 20:44:21 -0400

    Saving progress for Budget App final project
    Signed-off-by: SeraWing <swing@jeemail.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,11 @@
-
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget,currency } = useContext(AppContext);
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency}{parseInt(budget)}</span>
+        </div>
+    );
+};
+export default Budget;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,52 @@
+import React, { useContext } from 'react';
+import { FaTimesCircle } from 'react-icons/fa';
+import { FaMinusCircle } from 'react-icons/fa';
+import { FaPlusCircle } from 'react-icons/fa';
+import { AppContext } from '../context/AppContext';
 
+const ExpenseItem = (props) => {
+    const { dispatch,currency } = useContext(AppContext);
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
+    };
+
+    const increaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+}
+
+    return (
+        <tr>
+        <td>{props.name}</td>
+        <td>{currency}{parseInt(props.cost)}</td>
+        <td><FaPlusCircle size= '2.2em' color= 'green' onClick={ ()=> increaseAllocation(props.name)}></FaPlusCircle></td>
+        <td><FaMinusCircle size= '2.2em' color= 'red' onClick={ ()=> decreaseAllocation(props.name)}></FaMinusCircle></td>
+        <td><FaTimesCircle size= '1.5em' onClick={handleDeleteExpense}></FaTimesCircle></td>
+        </tr>
+    );
+};
+
+export default ExpenseItem;
